b8c6d74439d2f8d7684b2723a84b7dc3f6a76ef5
Jared Cuffe <jcuffe@gmail.com>
2017-11-27 21:17:26 -0800

    Changed timer

--- scripts/background-changer.py
+++ scripts/background-changer.py
@@ -6,4 +6,4 @@ import time
 while True:
     for path in [image.path for image in os.scandir(sys.argv[1])]:
         os.system('feh --bg-center --no-fehbg ' + path)
-        time.sleep(60 * 10) # every 10 minutes
+        time.sleep(60 * 15) # every 15 minutes
