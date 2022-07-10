66c43385f5d3e6831e5431251b0c73c2392c142b
Jared Cuffe <jcuffe@gmail.com>
2022-07-10 14:43:12 -0700

    Adds ansible installation script.

---
+++ .chezmoiscripts/run_once_install_ansible.sh
@@ -0,0 +1,5 @@
+# Install the ansible toolset, which we'll use to provision our machine
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
