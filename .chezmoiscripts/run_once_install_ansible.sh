bd3513ee7fab892e8f5db151bcea1711e0728d1c
Jared Cuffe <jcuffe@gmail.com>
2022-07-10 14:43:57 -0700

    Adds shebang.

--- .chezmoiscripts/run_once_install_ansible.sh
+++ .chezmoiscripts/run_once_install_ansible.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 # Install the ansible toolset, which we'll use to provision our machine
+
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
