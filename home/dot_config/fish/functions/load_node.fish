857ff05e70b72fd8e044b9837cf251a3459b03df
Jared Cuffe <jcuffe@gmail.com>
2022-07-10 11:38:56 -0700

    Delegates plugin management to fisher.

--- home/dot_config/fish/functions/load_node.fish
+++
@@ -1,4 +0,0 @@
-function load_node -v PWD
-    # TODO Check for nvmrc, nvm, etc
-    nvm use
-end
--- home/dot_config/fish/functions/sql.fish
+++
@@ -1,3 +0,0 @@
-function sql
-    mysql -h localhost.aptible.in -P 21777 -u aptible --password=jfGRjHXSrTFl8d5WdRWe7rCyVwIYG_u4 --safe-updates -D db
-end
